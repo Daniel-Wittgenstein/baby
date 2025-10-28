44324f723ea4088a0f0e2294abf09cfd2531d2e0
d o <@>
2025-10-28 18:10:51 +0100

    add new test

--- src/tests/basic.test.ts
+++ src/tests/basic.test.ts
@@ -11,6 +11,45 @@ compilerSetOnError((msg: string, line: Line) => {
   throw new Error(msg)
 })
 
+const BASIC_STORY =     `
+
+###story###
+
+You are walking through a forest.
+
+You reach a fork in the road.
+
+. Go left.
+
+  You go left and die.
+
+. Go right.
+
+  You go right and die.
+
+. Go back home.
+
+  You go back home: good decision!
+
+  In fact, this was the best decision.
+
+-
+
+The story ends here.
+
+`
+
+
+test("basic flow", () => {
+  expect(() => compileAndRun(
+    BASIC_STORY, {
+      clickOn: ["Go right."],
+      textAppears: ["You go right and die."],
+      textDoesNotAppear: [],
+    }
+  )).not.toThrowError()
+})
+
 
 test("basic code that should work without an error", () => {
   expect(() => compileAndRun(
@@ -32,4 +71,5 @@ test("basic code that should throw compiler error", () => {
     }
   )).toThrowError()
 })
+
 
